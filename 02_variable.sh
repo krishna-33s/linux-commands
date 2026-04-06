bd9716faff192b2a1b8076a18406d34b4d5f2215
krishna <venkatakrishnareddy450@gmailcom>
2026-04-06 12:09:49 +0530

    persons changed

---
+++ 02_variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$1="ramesh"
+$2="rajesh"
+
+echo "$1:: hii $2! how are you!"
+echo "$2:: fine da what about you" 
+echo "$1:: where are you right now"
+echo "$2:: i am currently staying at hyderabad"
