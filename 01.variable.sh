e3e8140cf8920a530eb68fd8c45ebcf9d589996b
krishna <venkatakrishnareddy450@gmailcom>
2026-04-06 08:35:59 +0530

    conversation

---
+++ 01.variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "krishna: hii ravi! how are you!"
+echo "ravi   : fine da what about you" 
+echo "krishna: where are you right now"
+echo "ravi   : i am currently staying at hyderabad"
+echo "krishna: ohh! what about jobs are you searching"
+echo "ravi   : no da the jobs are very tough in outside and they are asking more skills"
+echo "krishna: yaahhh thaat is correct"
