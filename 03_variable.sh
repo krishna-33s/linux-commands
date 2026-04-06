f11c73eb0828c88bb85b00e457bdb4037e2fea4b
krishna <venkatakrishnareddy450@gmailcom>
2026-04-06 12:21:56 +0530

    persons changed

---
+++ 03_variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: hii $person2 ! how are you!"
+echo "$person2:: fine da what about you" 
+echo "$person1:: where are you right now"
+echo "$person2:: i am currently staying at hyderabad"
