e5036739a767b4799b539751b2981763ae1cb4de
krishna <venkatakrishnareddy450@gmailcom>
2026-04-06 12:47:46 +0530

    username and passwd

--- 04_variable.sh
+++ 04_variable.sh
@@ -5,6 +5,6 @@ echo "pls enter user_name:"
 read user_name
 echo "user_name is : $user_name"
 
-echo "pls enter passwd:"
-read passwd
-echo "passwd is $passwd"
+echo "pls enter password:"
+read -s password
+echo "password is $password"
