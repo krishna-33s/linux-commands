1f12ad34395bae3235dc7a6fe89f5796da062705
krishna <venkatakrishnareddy450@gmailcom>
2026-04-06 12:31:19 +0530

    conversation

--- 01.variable.sh
+++ 01.variable.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-$1="ramesh"
-$2="rajesh"
 
-echo "$1:: hii $2! how are you!"
-echo "$2:: fine da what about you" 
-echo "$1:: where are you right now"
-echo "$2:: i am currently staying at hyderabad"
+echo "krishna: hii ravi! how are you!"
+echo "ravi   : fine da what about you" 
+echo "krishna: where are you right now"
+echo "ravi   : i am currently staying at hyderabad"
+echo "krishna: ohh! what about jobs are you searching"
+echo "ravi   : no da the jobs are very tough in outside and they are asking more skills"
+echo "krishna: yaahhh thaat is correct"
