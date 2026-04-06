1d1af9b68a7873bdca73644d9a13fb3a8a10f43a
krishna <venkatakrishnareddy450@gmailcom>
2026-04-06 09:15:20 +0530

    conversation

--- command-new.sh
+++ command-new.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
 #initial my name printing printing
+
 echo "hello krishna!"
-
-echo "lets prepare"
-
-echo "basic linux-commands"
-
-echo "pwd =present working directory"
-
-echo "cd = change directory"
-
-echo "after we have to add path"
-
-like cd /c/home/devops/krishna-88s--
-
