f85ea96540acc3b4dbab1591fd77a6ed9592ef87
krishna <venkatakrishnaredd450@gmail.com>
2026-04-09 14:08:16 +0530

    loop

---
+++ 12_loop.sh
@@ -0,0 +1,26 @@
+user_id=$(id -u)
+log_folder="/var/log/shell"
+log_file="/var/log/shell/$0.log"
+
+if [ $user_id -ne 0 ]; then
+    echo "pls run the script to root user" | tee -a $log_file
+    exit 1
+
+fi
+
+mkdir -p $log_folder
+
+Validate(){
+    if [ $1 -ne 0 ]; then
+        echo "$2...failed" | tee -a $log_file
+        exit 1
+    else
+        echo "$2...success" | tee -a $log_file
+    fi
+}
+
+for pack in $@
+do 
+    dnf install $pack -y &>>$log_file
+    Validate $? "$pack install"
+done    
