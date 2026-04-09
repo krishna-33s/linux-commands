a494a0de851da149bfaab4259526a2a90949494c
krishna <venkatakrishnaredd450@gmail.com>
2026-04-09 10:52:10 +0530

    installing version

--- 09_install.sh
+++ 09_install.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+user-id=$(id -u)
+
+if [ $user-id -ne 0 ]; then 
+    echo "please run with root user"
+    exit 1
+if    
+
+echo "installing nginx"
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "installing nginx...failed"
+else
+    echo "installing nginx...success"
+fi
+
