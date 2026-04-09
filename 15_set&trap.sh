e10965a76e191b416f0a939fbc40b51d00c7be23
krishna <venkatakrishnaredd450@gmail.com>
2026-04-09 18:03:55 +0530

    color

--- 15_set&trap.sh
+++ 15_set&trap.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 set -e
 trap "echo'error occured in $LINENO'" ERR
+
 user_id=$(id -u)
 log_folder="/var/log/shell"
 log_file="/var/log/shell/$0.log"
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 if [ $user_id -ne 0 ]; then
-    echo "pls run the script to root user" | tee -a $log_file
+    echo -e "$R pls run the script to root user $N" | tee -a $log_file
     exit 1
 
 fi
 
 mkdir -p $log_folder
 
+
+
 for pack in $@
 do 
     dnf list installed $pack &>>$log_file
     if [ $? -ne 0 ]; then 
-        echo "$pack is not installed ,installing now"
+        echo -"$pack is not installed ,installing now"
         dnf install $pack -y &>>$log_file
     else 
-        echo "$pack is already installed ,soo bye"
+        echo -e "$pack is already installed ,$R soo bye $N"
     fi        
-done    
+done
