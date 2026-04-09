62133d2724e4a39722d395814eac60351e84dc42
krishna <venkatakrishnaredd450@gmail.com>
2026-04-09 11:59:45 +0530

    function

---
+++ 10_functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+user_id=$(id -u)
+
+if [ $user_id -ne 0 ]; then
+    echo"pls run the script to root user"
+    exit 1
+
+fi
+
+Validate(){
+    if [ $1 -ne 0 ]; then
+        echo "$2...failed"
+    else
+        echo "$2...success"
+    fi
+}
+
+dnf install nginx -y
+validate $? "installing nginx"
+
+dnf install mysql -y
+validate $? "installing mysql"
+
+dnf install nodejs -y
+validate $? "installing nodejs"
