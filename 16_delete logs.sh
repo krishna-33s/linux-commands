fa20beda05d62cec2f180045ffd7be2e4789e1cf
krishna <venkatakrishnaredd450@gmail.com>
2026-04-16 16:12:40 +0530

    changes

---
+++ 16_delete logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+log_dir="/home/ec2-user/app-log"
+log_file="$log_dir/$0.log"
+
+
+if [ ! -d $log_dir]; then 
+    echo "directory does not exist"
+    exit 1
+fi
+
+find_files=$(find $log_dir -name "*.log" -mtime +14)
+echo "$find_files"
